de95d465ffd142e6e049117ad7aa5e73b0a20e45
Jacky <jackycky31@gmail.com>
2025-09-23 14:51:42 -0400

    code fixed

---
+++ src/apiCalls/photographer/studioService.js
@@ -0,0 +1,6 @@
+export const getAllPhotographersByStudio = (studioID) => {
+    const photographer_list = [
+        'Select photographer','Elias Thorne','Willow Hayes','Caleb Sterling','Aurora Vance'
+    ]
+    return photographer_list
+}
--- src/pages/photographer/createAlbum/createAlbum.jsx
+++ src/pages/photographer/createAlbum/createAlbum.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faImage } from "@fortawesome/free-solid-svg-icons"
+import { faImage, faArrowDown, faArrowUp } from "@fortawesome/free-solid-svg-icons"
 import { useSharedContext } from '../../../SharedContext';
 
 
@@ -9,194 +9,35 @@ import './createAlbum.css'
 import useRouteParams from '../../../hooks/useRouteParams';
 import { Album } from '../album/album';
 import { AlbumComponent } from '../../../components/albumComponent/albumComponent';
-
-
-const photographer_list = [
-    'Select photographer','abc','def','ghi'
-]
+import { getAllPhotographersByStudio } from '../../../apiCalls/photographer/studioService';
+import { CreateAlbumForm } from './createAlbumForm';
 
 export const CreateAlbum = () => {
-    const [formData, setFormData] = useState({
-        client_first_name: '',
-        client_last_name: '',
-        photo_shoot_location: '',
-        photo_shoot_date: '',
-        album_creation_date: '',
-        photographers: []
-    })
-    const [photographerComponent, setPhotographerComponent] = useState([])
+    const [formHeightRatio, setFormHeightRatio] = useState(0.7);
 
-    const handleInputChange = (e) => {
-        setFormData({
-        ...formData,
-        [e.target.name]: e.target.value,
-        });
-    };
-
-    const handleDateChange = (e) => {
-        let value = e.target.value.replace(/\D/g, ""); // keep only numbers
-
-        // Auto insert slashes
-        if (value.length > 2 && value.length <= 4) {
-        value = value.slice(0, 2) + "/" + value.slice(2);
-        } else if (value.length > 4) {
-        value = value.slice(0, 2) + "/" + value.slice(2, 4) + "/" + value.slice(4, 8);
-        }
-
-        setFormData({
-            ...formData,
-            [e.target.name]: value,
-        });
-    };
-
-    const handlePhotographerChange = (e) => {
-        if (!photographer_list.includes(e.target.value)) {return}
-        const photographer = photographer_list.find((photographer) => photographer === e.target.value);
-        setFormData({
-            ...formData,
-            photographers: [...formData.photographers, photographer]
-        })
-    };
-
-    useEffect(() => {
-        let photographerComponent_temp = []
-        for (let i=0; i <= formData.photographers.length; i++) {
-            photographerComponent_temp.push(
-                <div className="d-flex align-items-center gap-3 mb-2">
-                    <label>{i+1}.</label>
-                    <select
-                        name="photographer_name"
-                        value={formData.photographers[i]?formData.photographers[i]:''}
-                        onChange={handlePhotographerChange}
-                        className="form-select"
-                        style={{width: '15rem'}}
-                    >
-                        {photographer_list.map((photographer) => (
-                        <option value={photographer}>
-                            {photographer}
-                        </option>
-                        ))}
-                    </select>
-                </div>
-            )
-        }
-        setPhotographerComponent(photographerComponent_temp)
-    }, [formData.photographers])
+    const handleResize = () => {
+        if (formHeightRatio===0.7) {setFormHeightRatio(0.2)}
+        else if (formHeightRatio===0.2) {setFormHeightRatio(0.7)}
+    }
 
     return (
         <div className='p-0'>
             <div className='d-flex flex-column vh-100'>
-                <div className='sticky-top flex-shrink-0 border-bottom p-4'
-                    style={{maxHeight: '40%', overflow: 'auto'}}>
-                    <div className='mx-4 mt-1'>
-                        <input className='album_title_input px-3 bg-transparent' placeholder='Untitled Album' style={{fontSize: '2.5rem'}}/>
-                    </div>
-                    <div className='mx-4 mt-3'>
-                        <p className='custom-bold-text'>Client Information</p>
-                        <div className='d-flex flex-wrap mb-3'>
-                            <div className='form-input-group'>
-                                <input
-                                    type="text"
-                                    name="client_first_name"
-                                    value={formData.client_first_name}
-                                    onChange={handleInputChange}
-                                    placeholder=" "
-                                    className='form-input ps-3'
-                                />
-                                <label className='form-label ms-2'><small>First name</small></label>
-                            </div>
-                            <div className='form-input-group'>
-                                <input
-                                    type="text"
-                                    name="client_last_name"
-                                    value={formData.client_last_name}
-                                    onChange={handleInputChange}
-                                    placeholder=" "
-                                    className='form-input ps-3'
-                                />
-                                <label className='form-label ms-2'><small>Last name</small></label>
-                            </div>
-                            <div className='form-input-group'>
-                                <input
-                                    type="text"
-                                    name="client_email"
-                                    value={formData.client_email}
-                                    onChange={handleInputChange}
-                                    placeholder=" "
-                                    className='form-input ps-3'
-                                />
-                                <label className='form-label ms-2'><small>Email</small></label>
-                            </div>
-                            <div className='form-input-group'>
-                                <input
-                                    type="text"
-                                    name="client_phone"
-                                    value={formData.client_phone}
-                                    onChange={handleInputChange}
-                                    placeholder=" "
-                                    className='form-input ps-3'
-                                />
-                                <label className='form-label ms-2'><small>Phone Number</small></label>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className='mx-4 mt-3'>
-                        <p className='custom-bold-text'>Album Information</p>
-                        <div className='d-flex flex-wrap'>
-                            <div className='form-input-group'>
-                                <input
-                                    type="text"
-                                    name="photo_shoot_location"
-                                    value={formData.photo_shoot_location}
-                                    onChange={handleInputChange}
-                                    placeholder=" "
-                                    className='form-input ps-3'
-                                />
-                                <label className='form-label ms-2'><small>Photo Shoot Location</small></label>
-                            </div>
-                            <div className='form-input-group'>
-                                <input
-                                    type="text"
-                                    name="photo_shoot_date"
-                                    value={formData.photo_shoot_date}
-                                    onChange={handleDateChange}
-                                    placeholder="mm/dd/yyyy"
-                                    maxLength={10}
-                                    className='form-input ps-3'
-                                ></input>
-                                <label className='form-label ms-2'><small>Photo Shoot Date</small></label>
-                            </div>
-                            <div className='form-input-group'>
-                                <input
-                                    type="text"
-                                    name="album_creation_date"
-                                    value={formData.album_creation_date}
-                                    onChange={handleDateChange}
-                                    placeholder="mm/dd/yyyy"
-                                    className='form-input ps-3'
-                                />
-                                <label className='form-label ms-2'><small>Album Creation Date</small></label>
-                            </div>
-                            <div className='form-input-group'>
-                                <input
-                                    type="text"
-                                    name="client_phone"
-                                    value={formData.client_phone}
-                                    onChange={handleInputChange}
-                                    placeholder=" "
-                                    className='form-input ps-3'
-                                />
-                                <label className='form-label ms-2'><small>Phone Number</small></label>
-                            </div>
-                        </div>
-                        <div className='ms-3 mt-3'>
-                            <p>Photographers:</p>
-                            {photographerComponent}
-                        </div>
-                    </div>
+                <div className="d-flex flex-column" 
+                    style={{ flexBasis: formHeightRatio * 100 + "%", overflow: "hidden", transition: '0.3s' }}
+                >
+                <CreateAlbumForm studioID={123} />
                 </div>
-                <div className='w-100 flex-grow-1 d-flex flex-column overflow-hidden vh-50'>
+
+                <div className="resizing-button d-flex justify-content-center w-100 py-2 border-bottom"
+                    onClick={handleResize}
+                    role='button'
+                >
+                <FontAwesomeIcon icon={formHeightRatio < 0.5 ? faArrowDown : faArrowUp} />
+                </div>
+                <div className='w-100 d-flex flex-column overflow-hidden'
+                style={{ flexBasis: (1 - formHeightRatio) * 100 + "%", transition: '0.3s' }}
+                >
                     <AlbumComponent albumId={null}/>
                 </div>
             </div>
