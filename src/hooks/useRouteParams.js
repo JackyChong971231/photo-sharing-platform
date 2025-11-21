c463eba21e1a70a9256a3651d0282494b7761788
Jacky <chongkinye@gmail.com>
2025-11-21 11:22:27 -0500

    try

--- src/hooks/useRouteParams.js
+++
@@ -1,7 +0,0 @@
-import { useParams } from "react-router-dom";
-
-const useRouteParams = () => {
-  return useParams(); // Returns an object with all route params
-};
-
-export default useRouteParams;
